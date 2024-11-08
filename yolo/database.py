6bdc56e12214b2479828e0cd11db4c6448b3fbd7
DilumFernando <dilumamilafdo1995@gmail.com>
2024-11-07 20:29:44 -0500

    everything

--- yolo/database.py
+++ yolo/database.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, jsonify, current_app
 import psycopg
 import psycopg.rows
+
 database_service = Blueprint("database", __name__)
+
+@database_service.route("/connect_postgres", methods=["POST"])
 def connect_postgres() -> psycopg.Connection[psycopg.rows.TupleRow]:
     database_client = psycopg.connect(
         host=current_app.config["POSTGRES_HOST"],
@@ -11,27 +14,26 @@ def connect_postgres() -> psycopg.Connection[psycopg.rows.TupleRow]:
         dbname=current_app.config["POSTGRES_DB"],
         row_factory=psycopg.rows.dict_row
     )
-    return database_client
-@database_service.route("/create_table", methods=["POST"])
-def create_table():
-    with connect_postgres() as client:
-        cursor = client.cursor()
+    # Create the table if it doesn't exist
+    with database_client.cursor() as cursor:
         cursor.execute(
-            "CREATE TABLE classes (id UUID PRIMARY KEY, name TEXT)"
+            "CREATE TABLE IF NOT EXISTS classes (id UUID PRIMARY KEY, name TEXT)"
         )
+    return database_client
+
 @database_service.route("/get_table", methods=["GET"])
 def get_table():
     with connect_postgres() as client:
         cursor = client.cursor()
         cursor.execute("SELECT * FROM classes")
         return jsonify(cursor.fetchall())
+
 @database_service.route("/add_class", methods=["POST"])
-def add_class():
+def upload_video():
     with connect_postgres() as client:
         cursor = client.cursor()
         data = request.form
         cursor.execute(
             "INSERT INTO classes (id, name) VALUES (gen_random_uuid(), %s)",
-            (data.get("class"),)
+            (data.get("class"))
         )
-    return jsonify({"message": "Class added successfully"}), 201
